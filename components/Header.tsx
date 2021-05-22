747f383bbefa5f19553ec150712a4f4874c94da2
Jamie Emerson <inghamemerson@gmail.com>
2021-05-22 19:59:43 -0400

    update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,7 @@ export const Header = ({
   description,
   image
 }: IHeader) => (
-  <div className="relative bg-indigo-800">
+  <div className="relative bg-yellow-500">
     <div className="absolute inset-0">
       {image && (
         <img
@@ -20,7 +20,7 @@ export const Header = ({
           alt=""
         />
       )}
-      <div className="absolute inset-0 bg-indigo-800 mix-blend-multiply" aria-hidden="true" />
+      <div className="absolute inset-0 bg-yellow-600 mix-blend-multiply" aria-hidden="true" />
     </div>
     <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
       {title && (<h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">{title}</h1>)}
--- lib/index.ts
+++ lib/index.ts
@@ -9,3 +9,29 @@ export const fatalityRatio = ({ total_cases, total_deaths }: IFatalityRation): n
   };
   return 100;
 };
+
+interface IdeathPercentage {
+  population: number | null
+  total_deaths: number | null
+}
+
+export const deathPercentage = ({ population, total_deaths }: IdeathPercentage): number => {
+  if (population && total_deaths) {
+    return ((total_deaths / population) * 100);
+  };
+  return 100;
+};
+
+interface IVaccinePercentage {
+  population: number | null;
+  total_vaccinations: number | null;
+}
+
+export const vaccinePercentage = ({ population, total_vaccinations }: IVaccinePercentage): number => {
+  if (population && total_vaccinations) {
+    return ((total_vaccinations / population) * 100);
+  };
+  return 100;
+};
+
+// export const composite
